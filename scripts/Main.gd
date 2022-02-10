b5b1f7bfdb3e828ffa7afc17e7c38a08c3a53132
Martin Cerdeira <lisandrolorea@gmail.com>
2022-02-10 16:01:56 -0300

    prueba

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,7 +2,6 @@ extends Node2D
 onready var button = preload("res://scenes/tile_button.tscn")
 onready var icon1 = preload("res://sprites/path1.png")
 onready var icon2 = preload("res://sprites/warrior1.png")
-#
 
 func _ready():
 	$tile_positioner.texture = null
